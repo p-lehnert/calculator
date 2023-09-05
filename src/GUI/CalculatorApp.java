eb9fdc94810b5c0c31388e7043983679cef422dd
p-lehnert <plehnert@uni-bremen.de>
2023-09-05 14:10:36 +0200

    square- clear-, erase-, arithmetic-buttons Funktionalität hinzugefügt

--- src/GUI/CalculatorApp.java
+++ src/GUI/CalculatorApp.java
@@ -26,26 +26,33 @@ public class CalculatorApp extends JFrame implements ActionListener {
         // first row
         JButton squareBtn = new JButton("x²");
         squareBtn.addActionListener(this::squareAction);
-        JButton rootBtn = new JButton("\u221A");
+        JButton rootBtn = new JButton("\u221a");
+        rootBtn.addActionListener(this::rootAction);
         JButton clearBtn = new JButton("C");
+        clearBtn.addActionListener(this::clearAction);
         JButton eraseBtn = new JButton("<-");
+        eraseBtn.addActionListener(this::eraseAction);
         // second row
-        JButton divisionBtn = new JButton("/");
+        JButton divisionBtn = new JButton("\u00f7");
+        divisionBtn.addActionListener(this::arithmeticAction);
         JButton sevenBtn = new JButton("7");
         JButton eightBtn = new JButton("8");
         JButton nineBtn = new JButton("9");
         // third row
-        JButton multiplyBtn = new JButton("x");
+        JButton multiplyBtn = new JButton("*");
+        multiplyBtn.addActionListener(this::arithmeticAction);
         JButton fourBtn = new JButton("4");
         JButton fiveBtn = new JButton("5");
         JButton sixBtn = new JButton("6");
         // fourth row
         JButton minusBtn = new JButton("-");
+        minusBtn.addActionListener(this::arithmeticAction);
         JButton oneBtn = new JButton("1");
         JButton twoBtn = new JButton("2");
         JButton threeBtn = new JButton("3");
         // fifth row
         JButton plusBtn = new JButton("+");
+        plusBtn.addActionListener(this::arithmeticAction);
         JButton commaBtn = new JButton(",");
         JButton zeroBtn = new JButton("0");
         JButton equalsBtn = new JButton("=");
@@ -86,17 +93,85 @@ public class CalculatorApp extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
-    public void squareAction (ActionEvent e) {
+    /**
+     * Adds a '²' to the given position (Caret-Position).
+     *
+     * @param e Actionevent that is triggered, when squareBtn is pressed
+     */
+    private void squareAction (ActionEvent e) {
+        // check whether textarea is empty
         if (!Objects.equals(textArea.getText(), "")) {
-
-            char lastChar = checkAndCut();
-
-            if (Character.isDigit(lastChar)) {
-                textArea.setText(textArea.getText() + "² ");
+            char charAtCaret = checkCharAtCaret();
+            int caretIndex = textArea.getCaretPosition();
+            if (Character.isDigit(charAtCaret)) {
+                String calc = textArea.getText().substring(0, caretIndex) + "²" + textArea.getText().substring(caretIndex);
+                textArea.setText(calc);
+                textArea.setCaretPosition(caretIndex + 1);
             }
         }
         textArea.requestFocus();
+    }
 
+    /**
+     * Adds a root-symbol and parenthesis.
+     *
+     * @param e Actionevent
+     */
+    private void rootAction (ActionEvent e) {
+        int caretIndex = textArea.getCaretPosition();
+
+        if (Objects.equals(textArea.getText(), "")) {
+            textArea.setText("\u221A()");
+        } else if (caretIndex == textArea.getText().length() - 1) {
+            textArea.setText(textArea.getText() + " \u221A()");
+            textArea.setCaretPosition(caretIndex + 1);
+        } else {
+            String calc = textArea.getText().substring(0, caretIndex)
+                    + "\u221A()" + textArea.getText().substring(caretIndex);
+            textArea.setText(calc);
+        }
+        textArea.requestFocus();
+        textArea.setCaretPosition(caretIndex + 2);
+    }
+
+    /**
+     * Clears textArea.
+     *
+     * @param e Actionevent
+     */
+    private void clearAction(ActionEvent e) {
+        textArea.setText("");
+        textArea.requestFocus();
+    }
+
+    /**
+     * Erases one Character.
+     *
+     * @param e Actionevent
+     */
+    private void eraseAction(ActionEvent e) {
+        int caretIndex = textArea.getCaretPosition();
+        if (!Objects.equals(textArea.getText(), "") && caretIndex > 0) {
+            String calc = textArea.getText().substring(0, caretIndex - 1) + textArea.getText().substring(caretIndex);
+            textArea.setText(calc);
+            textArea.setCaretPosition(caretIndex - 1);
+        }
+        textArea.requestFocus();
+    }
+
+    /**
+     * Adds the pressed arithmetic symbol.
+     *
+     * @param e Actionevent
+     */
+    private void arithmeticAction (ActionEvent e) {
+        JButton b = (JButton) e.getSource();
+        String bValue = b.getText();
+
+        int caretIndex = textArea.getCaretPosition();
+        textArea.setText(textArea.getText().substring(0, caretIndex) + bValue + textArea.getText().substring(caretIndex));
+        textArea.setCaretPosition(caretIndex + 1);
+        textArea.requestFocus();
     }
 
     @Override
@@ -104,17 +179,16 @@ public class CalculatorApp extends JFrame implements ActionListener {
 
     }
 
-    private char checkAndCut () {
+    /**
+     * Returns the last char of @textArea.
+     *
+     * @return last Character of textArea
+     */
+    private char checkCharAtCaret () {
         if(textArea.getText() == null) {
             return ' ';
+        } else {
+            return textArea.getText().charAt(textArea.getCaretPosition() - 1);
         }
-        char lastChar = textArea.getText().charAt(textArea.getText().length() - 1);
-        while (lastChar == ' ') {
-            lastChar = textArea.getText().charAt(textArea.getText().length() - 1);
-            textArea.setText(textArea.getText().substring(0, textArea.getText().length() - 1));
-        }
-        textArea.setText((textArea.getText() + lastChar));
-
-        return lastChar;
     }
 }
